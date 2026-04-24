f4452a674c919e038e5a50f92f2b076b4f9503d5
garuminn <gabyreyesurbina@gmail.com>
2026-04-24 14:36:54 -0500

    SE ACTUALIZA LA POSICION DE LOS HEROES DESPUES DE COMBATE
    ayuda no puedo mas

--- Parcial2-ComputacionGrafica/Assets/Scripts/GameManager.cs
+++ Parcial2-ComputacionGrafica/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
@@ -22,6 +23,7 @@ public class GameManager : MonoBehaviour
     public int heroesVivos;
     public string escenaRetornoCombate = "Juego";
     public string nombreContenedorHeroes = "Heroes";
+    public string nombreArchivoPosicionesHeroes = "heroes_posiciones.json";
 
     private Dictionary<int, Vector3> posicionesLocalesHeroesPorIndice = new Dictionary<int, Vector3>();
     private Vector3 posicionGrupoHeroesGuardada;
@@ -63,6 +65,7 @@ public class GameManager : MonoBehaviour
         SincronizarHeroesConEscena();
         GuardarPosicionGrupoHeroes();
         GuardarPosicionesHeroesEscena();
+        GuardarPosicionesEnJson();
         restaurarPosicionesPendiente = true;
         Debug.Log("Encuentro preparado. Escena retorno: " + escenaRetornoCombate);
     }
@@ -148,6 +151,7 @@ public class GameManager : MonoBehaviour
 
         if (restaurarPosicionesPendiente && scene.name == escenaRetornoCombate)
         {
+            CargarPosicionesDesdeJson();
             restaurarPosicionesPendiente = false;
             StartCoroutine(RestaurarPosicionesAlFinalFrame());
         }
@@ -205,6 +209,70 @@ public class GameManager : MonoBehaviour
         return null;
     }
 
+    void GuardarPosicionesEnJson()
+    {
+        HeroesPositionsData data = new HeroesPositionsData();
+        data.escenaRetorno = escenaRetornoCombate;
+        data.hayPosicionGrupoGuardada = hayPosicionGrupoGuardada;
+        data.posicionGrupo = new Vector3Data(posicionGrupoHeroesGuardada);
+
+        foreach (KeyValuePair<int, Vector3> entry in posicionesLocalesHeroesPorIndice)
+        {
+            data.heroes.Add(new HeroPositionData(entry.Key, entry.Value));
+        }
+
+        string carpeta = Application.streamingAssetsPath;
+        if (!Directory.Exists(carpeta))
+        {
+            Directory.CreateDirectory(carpeta);
+        }
+
+        string rutaArchivo = ObtenerRutaArchivoPosiciones();
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(rutaArchivo, json);
+
+        Debug.Log("Posiciones de heroes guardadas en: " + rutaArchivo);
+    }
+
+    void CargarPosicionesDesdeJson()
+    {
+        string rutaArchivo = ObtenerRutaArchivoPosiciones();
+        if (!File.Exists(rutaArchivo))
+        {
+            Debug.LogWarning("No se encontro archivo de posiciones de heroes en: " + rutaArchivo);
+            return;
+        }
+
+        string json = File.ReadAllText(rutaArchivo);
+        HeroesPositionsData data = JsonUtility.FromJson<HeroesPositionsData>(json);
+        if (data == null)
+        {
+            Debug.LogWarning("No se pudo leer el JSON de posiciones de heroes.");
+            return;
+        }
+
+        escenaRetornoCombate = string.IsNullOrEmpty(data.escenaRetorno) ? escenaRetornoCombate : data.escenaRetorno;
+        hayPosicionGrupoGuardada = data.hayPosicionGrupoGuardada;
+        posicionGrupoHeroesGuardada = data.posicionGrupo.ToVector3();
+
+        posicionesLocalesHeroesPorIndice.Clear();
+        if (data.heroes != null)
+        {
+            foreach (HeroPositionData heroe in data.heroes)
+            {
+                posicionesLocalesHeroesPorIndice[heroe.indice] = heroe.posicion.ToVector3();
+            }
+        }
+
+        hayPosicionesHeroesGuardadas = posicionesLocalesHeroesPorIndice.Count > 0;
+        Debug.Log("Posiciones de heroes cargadas desde: " + rutaArchivo);
+    }
+
+    string ObtenerRutaArchivoPosiciones()
+    {
+        return Path.Combine(Application.streamingAssetsPath, nombreArchivoPosicionesHeroes);
+    }
+
     void SincronizarHeroesConEscena()
     {
         Heroe[] heroesEscena = FindObjectsByType<Heroe>(FindObjectsSortMode.None);
@@ -247,5 +315,51 @@ public class GameManager : MonoBehaviour
     {
         ActualizarHeroesVivos();
         return heroesVivos == 0;
+    }
+}
+
+[System.Serializable]
+public class HeroesPositionsData
+{
+    public string escenaRetorno;
+    public bool hayPosicionGrupoGuardada;
+    public Vector3Data posicionGrupo = new Vector3Data();
+    public List<HeroPositionData> heroes = new List<HeroPositionData>();
+}
+
+[System.Serializable]
+public class HeroPositionData
+{
+    public int indice;
+    public Vector3Data posicion = new Vector3Data();
+
+    public HeroPositionData(int indiceHeroe, Vector3 posicionHeroe)
+    {
+        indice = indiceHeroe;
+        posicion = new Vector3Data(posicionHeroe);
+    }
+}
+
+[System.Serializable]
+public class Vector3Data
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public Vector3Data()
+    {
+    }
+
+    public Vector3Data(Vector3 vector)
+    {
+        x = vector.x;
+        y = vector.y;
+        z = vector.z;
+    }
+
+    public Vector3 ToVector3()
+    {
+        return new Vector3(x, y, z);
     }
 }
